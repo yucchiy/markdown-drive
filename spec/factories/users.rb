9f7994ae84bb64a624b04fb2def5c7474a520c6f
Yuichiro Mukai <y.iky917@gmail.com>
2014-08-17 21:15:15 +0900

    Revert "Merge branch 'user-auth'"
    This reverts commit 55b779284bd651e4236bccf846b47b1f4524ebed, reversing
    changes made to e6266c8b5aa774fce6f744d638d7c13461b9c7f4.

--- spec/factories/users.rb
+++
@@ -1,8 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :user do
-    email 'test@example.com'
-    password 'f4k3p455w0rd'
-  end
-end
