aea2da27379bcefe6d91cea68f92b380c4d3563f
Yuichiro Mukai <y.iky917@gmail.com>
2014-08-26 13:45:02 +0900

    Documentのテスト書いた

--- spec/factories/documents.rb
+++ spec/factories/documents.rb
@@ -2,5 +2,16 @@
 
 FactoryGirl.define do
   factory :document do
+    path "/Public/latest.txt"
+    rev "35c1f029684fe"
+    revision 220191
+    bytes 0
+    size "0 bytes"
+    is_deleted false
+    association :user
+
+    factory :document_deleted do
+      is_deleted true
+    end
   end
 end
--- spec/models/document_spec.rb
+++ spec/models/document_spec.rb
@@ -1,5 +1,52 @@
 require 'rails_helper'
 
 RSpec.describe Document, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory" do
+    expect(FactoryGirl.build(:document)).to be_valid
+  end
+
+  it "is invalid without path" do
+    expect(FactoryGirl.build(:document, path: nil)).to be_invalid
+  end
+
+  it "is invalid without rev" do
+    expect(FactoryGirl.build(:document, rev: nil)).to be_invalid
+  end
+
+  it "is invalid without revision" do
+    expect(FactoryGirl.build(:document, revision: nil)).to be_invalid
+  end
+
+  it "is invalid without bytes" do
+    expect(FactoryGirl.build(:document, bytes: nil)).to be_invalid
+  end
+
+  it "is invalid without size" do
+    expect(FactoryGirl.build(:document, size: nil)).to be_invalid
+  end
+
+  it "is invalid without is_deleted" do
+    expect(FactoryGirl.build(:document, is_deleted: nil)).to be_invalid
+  end
+
+  it "is_deleted should be boolean" do
+    expect(FactoryGirl.build(:document, is_deleted: "boolean")).to have(1).errors_on(:is_deleted)
+  end
+
+  it "is invalid without edited_at" do
+    expect(FactoryGirl.build(:document, edited_at: nil)).to be_invalid
+  end
+
+  it "edited_at should be valid datetime format" do
+    expect(FactoryGirl.build(:document, edited_at: "boolean")).to have(1).errors_on(:edited_at)
+  end
+
+  it "is invalid without user" do
+    expect(FactoryGirl.build(:document, user_id: nil)).to be_invalid
+  end
+
+  it "does not allow duplicate documents per user" do
+    FactoryGirl.create(:document)
+    expect(FactoryGirl.build(:document)).to be_invalid
+  end
 end
