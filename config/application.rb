466e9b3441f5c6e7a884e5dcc574be81081f43bd
Yuichiro Mukai <y.iky917@gmail.com>
2014-12-21 17:28:47 +0900

    change generator config

--- config/application.rb
+++ config/application.rb
@@ -31,8 +31,18 @@ module MarkdownDrive
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    config.app_generators do |c|
-      c.test_framework :rspec
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: true,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: true,
+        request_specs: false
+      g.fixture_replacement :factory_girl, dir: "spec/factories"
+
+      g.stylesheets = false
+      g.javascripts = false
     end
   end
 end
