58708aa0b13454ad7ec8bd839a778e5260428f2e
Yuichiro Mukai <y.iky917@gmail.com>
2014-12-23 12:23:51 +0900

    add a migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141221111057) do
+ActiveRecord::Schema.define(version: 20141223032110) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20141221111057) do
     t.string   "github_token", null: false
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.string   "avatar_url"
   end
 
   add_index "users", ["github_id"], name: "index_users_on_github_id", unique: true, using: :btree
