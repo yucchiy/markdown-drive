1d7b9d459196c15d840426801b08475c7511afb6
Yuichiro Mukai <y.iky917@gmail.com>
2014-12-24 14:18:32 +0900

    create a setting if a user does not have it

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,8 +2,9 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_or_create_by_auth_hash(auth_hash)
-
+    user.create_user_setting if user.user_setting == nil
     user.save
+
     sign_in user
     redirect_to user
   end
